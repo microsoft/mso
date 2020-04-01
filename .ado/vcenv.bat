b5287496ea7b9c30767d8e3ebaf16676d2182bfc
Adam Foxman <adam@thefoxmans.net>
2020-04-01 16:25:09 -0700

    Remove quotes around CC and CXX values. Echo values for easier debugging

--- .ado/vcenv.bat
+++ .ado/vcenv.bat
@@ -1,3 +1,5 @@
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" %BUILDPLATFORM% %BUILDWINDOWSSDK%
-SET CC="%VCToolsInstallDir%\bin\Hostx64\%VSCMD_ARG_TGT_ARCH%\cl.exe"
-SET CXX="%VCToolsInstallDir%\bin\Hostx64\%VSCMD_ARG_TGT_ARCH%\cl.exe"
+SET CC=%VCToolsInstallDir%\bin\Hostx64\%VSCMD_ARG_TGT_ARCH%\cl.exe
+SET CXX=%VCToolsInstallDir%\bin\Hostx64\%VSCMD_ARG_TGT_ARCH%\cl.exe
+ECHO CC=%CC%
+ECHO CXX=%CXX%
