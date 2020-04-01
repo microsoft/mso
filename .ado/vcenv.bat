f3d2c0349124424ccd9a9ba3d0e641d2a75098ab
Adam Foxman <adam@thefoxmans.net>
2020-04-01 15:56:35 -0700

    Set CC and CXX as hints for CMake when it runs on the CI machines.

---
+++ .ado/vcenv.bat
@@ -0,0 +1,3 @@
+CALL 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat' %BUILDPLATFORM% %BUILDWINDOWSSDK%
+SET CC="%VCToolsInstallDir%\bin\Hostx64\%VSCMD_ARG_TGT_ARCH%\cl.exe"
+SET CXX="%VCToolsInstallDir%\bin\Hostx64\%VSCMD_ARG_TGT_ARCH%\cl.exe"
