0944134bcef083fe26318cf9245ec2ab997e2cf7
Adam Foxman <adam@thefoxmans.net>
2020-04-01 17:52:02 -0700

    Configure windows builds to be strict. All warnings are on, and are treated as errors. To make this tolerable, update CMake minimum version to 3.15 and enable CMP0092 to remove /W3 from the baseline compiler flags.

--- tools/cmake/liblet.cmake
+++ tools/cmake/liblet.cmake
@@ -310,6 +310,10 @@ function(_liblet_set_platform_definitions TARGET)
   if (CMAKE_CXX_STANDARD EQUAL 17)
     target_compile_definitions(${TARGET} PRIVATE _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS)
   endif()
+
+  target_compile_options(${TARGET}
+    PRIVATE
+      $<$<CXX_COMPILER_ID:MSVC>:/permissive- /W4 /WX>)
 endfunction()
 
 function(_liblet_verify_includes LIBLET_TARGET LIBLET_INCLUDES)
